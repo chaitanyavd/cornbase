c95284b4165e0f9ece8f2306d5d06bd31661cd38
chaitanyavd <chaitanya.v.de@gmail.com>
2019-07-03 16:20:28 -0700

    add engines and postinstall

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    
+    if @user
+      login(@user)
+      render json: @user
+    else
+      render json: ["Invalid email/password combination"], status: 401
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+      render json: @user # Instructions say to render {} 
+    else
+      render json: ["Nobody signed in"], status: 404
+    end
+  end
+end
