6bd591785751e028ccfb594d96eca63283f3736e
chaitanyavd <chaitanya.v.de@gmail.com>
2019-07-03 19:37:53 -0700

    Add signup/login on one form

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'; 
-import GreetingContainer from './greeting/greeting_container'
+import {Route} from 'react-router-dom'; 
+import GreetingContainer from './greeting/greeting_container'; 
+import LoginFormContainer from './session_form/login_form_container'; 
+import SignupFormContainer from './session_form/signup_form_container'; 
+
 const App = () => (
     <div>
-        <h1>CooornBase</h1>
-        <GreetingContainer/>
+        <header>
+            <h3>CornBase</h3>
+            <GreetingContainer/>
+        </header>
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
+
     </div>
 )
 
