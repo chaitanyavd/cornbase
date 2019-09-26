f22e8b68c145abc17b23e3458b1a52cbda39c07a
chaitanyavd <chaitanya.v.de@gmail.com>
2019-09-26 14:32:46 -0700

    Add deposit table and model for transactions

---
+++ app/models/deposit.rb
@@ -0,0 +1,6 @@
+class Deposit < ApplicationRecord
+
+  validates :user_id, presence: true
+  belongs_to :user
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_003515) do
+ActiveRecord::Schema.define(version: 2019_09_26_212944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2019_09_18_003515) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ticker"], name: "index_coins_on_ticker", unique: true
+  end
+
+  create_table "deposits", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "amount", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_deposits_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
